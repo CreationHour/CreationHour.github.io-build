cf56fab3f11a11e8b9ad2d665e62288f28f0a7cf
Junwoo Shin <shin.junwoo97@gmail.com>
2016-04-25 19:34:41 -0400

    started on the home page

--- gulpfile.js
+++ gulpfile.js
@@ -145,6 +145,7 @@ gulp.task('wiredep:dist', function () {
 
 gulp.task('default', ['clean:srv', 'wiredep:srv', 'sass:srv', 'js:srv', 'images:srv', 'fonts:srv'], function() {
 	browserSync.init({
+		// FIXME: fix serving
 		server: './',
 		notify: false
 	});
